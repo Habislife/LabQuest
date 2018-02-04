c1f66366608fbccaf2b6e13306194f8cd2663153
asmit1 <34081059+asmit1@users.noreply.github.com>
2018-02-04 11:30:49 +0545

    Reverse a series of numbers

---
+++ Reverses.c
@@ -0,0 +1,21 @@
+//Reverse a series of Numbers
+#include<stdio.h>
+int main()
+{
+	int a[10],i ;
+	
+	//for storing value
+	printf("Enter any 10 numbers :\n");
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	
+	//for printing reversed series of numbers
+	printf("The reversed series of numbers are: ");
+	for(i=9;i>=0;i--)
+	{
+		printf("%d ",a[i]);
+	}
+	return 0 ;
+}
